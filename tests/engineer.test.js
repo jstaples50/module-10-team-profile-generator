07c4ac2d98ac90a4a353cc2be8c9bd036649b1a5
Jared Staples <jstaplesmusic@gmail.com>
2022-11-06 12:41:17 -0500

    Created Engineer class and test

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -2,6 +2,26 @@ const Engineer = require('../lib/engineer');
 
 describe('Engineer', () => {
     describe('Initialization', () => {
-        
+        it('should create an Engineer object with a github value and a value of Engineer for role', () => {
+            const engineer = new Engineer('Carry', 5566, 'carry@gmail.com', 'carry5060');
+
+            expect(engineer.github).toEqual('carry5060');
+            expect(engineer.role).toEqual('Engineer');
+        })
+
+        it('should throw an error if github is not a string', () => {
+            const cb = () => new Engineer('Carry', 5566, 'carry@gmail.com');
+            const err = new Error("Expected parameter 'github', to be a non-empty string");
+
+            expect(cb).toThrow(err);
+        })
+    })
+
+    describe('getGitHub', () => {
+        it('should return objects github value', () => {
+            const employee = new Engineer('Vance', 5776, 'vance@gmail.com', 'vance-gith');
+
+            expect(employee.getGitHub()).toEqual('vance-gith');
+        })
     })
 })
